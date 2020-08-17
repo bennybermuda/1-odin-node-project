c58383b22c5ed37e83034c0825a367ac308ae3ef
Jimmy Tahiti <jimmytahiti@Benny-Bermuda-Player-of-Triangles.local>
2020-08-17 01:45:30 -0700

    cleaned up using arrow syntax for functions

--- server.js
+++ server.js
@@ -4,23 +4,23 @@ var port = 8080;
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile('index.html')
 })
 
-app.get('/about', function(req, res) {
+app.get('/about', (req, res) => {
     res.sendFile('about.html')
 })
 
-app.get('/contact-me', function(req, res) {
+app.get('/contact-me', (req, res) => {
     res.sendFile('contact-me.html')
 })
 
-app.use(function (req,res,next){
+app.use((req,res,next) => {
     res.status(404).send('Unable to find the requested resource!');
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
     console.log('listening on port ' + port)
 })
 
