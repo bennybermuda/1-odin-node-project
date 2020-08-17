a47f86e37e684cfaa24a7a62b82b9bf0aa8a4028
Jimmy Tahiti <jimmytahiti@Benny-Bermuda-Player-of-Triangles.local>
2020-08-16 23:01:33 -0700

    rewrote server in express

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var success = 200;
+var express = require('express');
+var app = express();
 var port = 8080;
 
-http.createServer(function(req,res) {
-    var q = url.parse(req.url, true);
-    if (q.pathname == '/') {
-        q.pathname += 'index.html'
-    }
-    var filename = "." + q.pathname;
-    fs.readFile(filename, (err, data) => {
-        if (err) {
-            fs.readFile("./404.html", (err,data) => {
-                res.end(data);
-            })
-        } else {
-            res.writeHead(success, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-        }
-    });
-}).listen(port);
+app.use(express.static('public'));
 
-console.log('listening on port ' + port)
+app.get('/', function(req, res) {
+    res.sendFile('index.html')
+})
+
+app.get('/about', function(req, res) {
+    res.sendFile('about.html')
+})
+
+app.get('/contact-me', function(req, res) {
+    res.sendFile('contact-me.html')
+})
+
+app.listen(port, function() {
+    console.log('listening on port ' + port)
+})
+
